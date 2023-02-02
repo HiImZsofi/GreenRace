4afb7f49a6b74d92b0138f3bc76e10a2156cfd4d
HiImZsofi <zsofia.gyori00@gmail.com>
2023-02-02 13:44:10 +0100

    fix subrepo

---
+++ client/src/App.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { response } from 'express';
+
+function App() {
+
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+  
+    useEffect(() => {
+      fetch('http://localhost:3306/api')
+        .then(response => response.json())
+        .then(data => setData(data))
+        .catch(error => setError(error));
+    }, []);
+  
+    return (
+      <div>
+        <>
+        {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'Loading...'}
+        </>
+      </div>
+    );
+}
+
+
+export default App;
