c832a14bf33f7527455a1db6b73fe3af6f709f1a
Gondi László <laszlo.zsolt.gondi@gmail.com>
2023-02-03 15:54:36 +0100

    Add Login button to login form

--- client/src/Login.tsx
+++ client/src/Login.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import FormSubmitButton from "./components/FormSubmitButton";
 import InputField from "./components/InputField";
 
 class LoginForm extends React.Component {
@@ -7,6 +8,7 @@ class LoginForm extends React.Component {
 			<React.StrictMode>
 				<InputField inputType={"Username"} />
 				<InputField inputType={"Password"} />
+				<FormSubmitButton inputType={"Login"} />
 			</React.StrictMode>
 		);
 	}
