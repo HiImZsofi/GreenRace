4ce8e00e7629e207b98abc1c71afcfb780183792
Gondi László <laszlo.zsolt.gondi@gmail.com>
2023-02-03 13:35:37 +0100

    Add POST method for login

---
+++ server/login.js
@@ -0,0 +1,36 @@
+//Imports
+const express = require("express");
+
+//Database config
+//? Maybe extract it to another js file for easier reuse
+const PORT = process.env.PORT || 3306;
+const app = express();
+const cors = require("cors");
+app.use(cors());
+
+//User POSTs info to the backend
+app.post("/login", (req, res) => {
+	//Store data in from the POST request
+	const { username, password } = req.body;
+	//Try username againts the database
+	try {
+		const { passwordInDB } = connection
+			.selectFrom(user)
+			.where(user.username.equals(username))
+			.select({
+				username: user.username,
+				password: user.password,
+			})
+			.executeSelectOne();
+	} catch (error) {
+		res.send("Incorrect username");
+	}
+
+	//Check password againts the one fetched from the database
+	if (password === passwordInDB) {
+		//TODO: decrypt encrypted password
+		res.send("Successful login");
+	} else {
+		res.send("Incorrect password");
+	}
+});
