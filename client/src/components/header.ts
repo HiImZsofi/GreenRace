d1737424acefa9254a03bccd37e8237992f46b9f
HiImZsofi <zsofia.gyori00@gmail.com>
2023-02-03 09:06:32 +0100

    registration form setup

---
+++ client/src/components/header.ts
@@ -0,0 +1,5 @@
+import React from "react";
+
+function header(){
+    //create a header element for the registration page
+}
---
+++ client/src/components/registrationForm.ts
@@ -0,0 +1,6 @@
+import React, {useState} from 'react';
+
+function RegistrationForm(){
+    //create the registration form itself here
+    //import it to app.tsx
+}
