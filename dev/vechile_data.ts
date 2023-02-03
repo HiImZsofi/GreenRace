2c590f5f3f37e1272b2b4ef5671fc0c45c0a6974
Gondi László <laszlo.zsolt.gondi@gmail.com>
2023-02-03 12:10:13 +0100

    Filter through the list and change the values where necessary

--- dev/vechile_data.ts
+++ dev/vechile_data.ts
@@ -1,10 +1,17 @@
 //Imports
 import fs from 'fs';
-
 var path = require("path");
+// const mysql = require('mysql')
 
 
 //!connect to database
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'my_db'
+// })
+// connection.connect()
 
 
 class Vehicle{
@@ -21,14 +28,32 @@ class Vehicle{
 
 const fileData = fs.readFileSync(path.resolve(__dirname, "buses.txt"), 'utf-8');
 const dataArray = fileData.split('\n');
-const vehicles = dataArray.map((line) => {
+let vehicles = dataArray.map((line) => {
 	const [model, fuelConsumption, fuelType] = line.split(';');
 	return new Vehicle(model, parseInt(fuelConsumption), fuelType);
 });
 
 console.log(vehicles);
 
+const dieselConsumption = vehicles.filter(x=>x.fuelType=='diesel\r' && x.fuelConsumption!=-1).map(v=>v.fuelConsumption)
+let avg:number=0;
+dieselConsumption.forEach(c => {
+	avg += c;
+});
+console.log(avg)
+avg/=dieselConsumption.length;
 
+vehicles.filter(x=>x.fuelType='diesel\r').filter(neg=>neg.fuelConsumption=-1).map(val=> val.fuelConsumption = avg)
+
+console.log(vehicles)
+
+// vehicles.forEach(v => {
+// 	const insertCustomer = connection.insertInto('vehicle').set({
+//         model: v.model,
+//         fuel_consumption: v.fuelConsumption,
+//         fuel_type: v.fuelType
+//     }).executeInsert(); 
+// });
 
 
 
