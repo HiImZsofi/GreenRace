7fa40a1cd9142745b10152393fffdda4b790ac94
Benedek Ákos <benedek.akos.marcell@gmail.com>
2023-02-03 16:25:40 +0100

    database creation: routes table creation

--- dev/database/createTable.js
+++ dev/database/createTable.js
@@ -1,8 +1,8 @@
-//Imports
+// Imports
 var mysql = require('mysql2');
 var express = require('express');
 
-//Connect to database
+// Connect to database
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -13,7 +13,7 @@ const connection = mysql.createConnection({
 connection.query(
     'SET FOREIGN_KEY_CHECKS=0;',
 );
-//Create users table
+// Create users table
 connection.query(
     'CREATE OR REPLACE TABLE Users (user_ID VARCHAR(255) PRIMARY KEY, username VARCHAR(30) NOT NULL, password VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, points INT, picfilename VARCHAR(255), picfilepath VARCHAR(255))',
     function(err, result){
@@ -22,16 +22,16 @@ connection.query(
     }
 );
 
-//Create friends table
+// Create friendships table
 connection.query(
     'CREATE OR REPLACE TABLE Friendships (friendship_ID VARCHAR(255) PRIMARY KEY, user_ID VARCHAR(255) NOT NULL, friend_id VARCHAR(255) NOT NULL, CONSTRAINT fk_fuuser_id FOREIGN KEY (user_ID) REFERENCES Users (user_ID), CONSTRAINT fk_ffuser_id FOREIGN KEY (friend_ID) REFERENCES Users (user_ID))',
     function(err, result){
         if(err) throw err;
-            console.log("Table Friends created or modified");    
+            console.log("Table Friendships created or modified");    
     }
 );
 
-//Create achivements table
+// Create achivements table
 connection.query(
     'CREATE OR REPLACE TABLE Achivements (achivement_ID VARCHAR(255) PRIMARY KEY)',
     function(err, result){
@@ -40,7 +40,7 @@ connection.query(
     }
 );
 
-//Create Completions table
+// Create Completions table
 connection.query(
     'CREATE OR REPLACE TABLE Completions (completion_ID VARCHAR(255) PRIMARY KEY, user_ID VARCHAR(255) NOT NULL, achivement_ID VARCHAR(255) NOT NULL, completion_date DATETIME NOT NULL, CONSTRAINT fk_cuser_id FOREIGN KEY (user_ID) REFERENCES Users (user_ID), CONSTRAINT fk_achi_id FOREIGN KEY (achivement_ID) REFERENCES Achivements (achivement_ID))',
     function(err, result){
@@ -50,4 +50,22 @@ connection.query(
     }
 );
 
-//process.exit();
+// Create Vehicles table
+connection.query(
+    'CREATE OR REPLACE TABLE Vehicles (vehicle_id VARCHAR(255) PRIMARY KEY, vehicle_type VARCHAR(30) NOT NULL, model VARCHAR(255), emission int NOT NULL)',
+    function(err, result){
+        if(err) throw err;
+            console.log("Table Vehicles created or modified");
+               
+    }
+);
+
+// Create Route table
+connection.query(
+    'CREATE OR REPLACE TABLE Routes (route_id VARCHAR(255) PRIMARY KEY, user_id VARCHAR(255) NOT NULL, vehicle_id VARCHAR(255) NOT NULL, length int, CONSTRAINT fk_ruser_id FOREIGN KEY (user_ID) REFERENCES Users (user_ID), CONSTRAINT fk_vehicle_id FOREIGN KEY (vehicle_id) REFERENCES Vehicles (vehicle_id))',
+    function(err, result){
+        if(err) throw err;
+            console.log("Table Routes created or modified");
+            process.exit();   
+    }
+);
