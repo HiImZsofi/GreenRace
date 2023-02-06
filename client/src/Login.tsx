50c37db2fdf15f836b2ddbad690cf142fea2793f
Gondi László <laszlo.zsolt.gondi@gmail.com>
2023-02-06 15:34:33 +0100

    Fix POST request in login not sending the correct data

--- client/src/Login.tsx
+++ client/src/Login.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import FormSubmitButton from "./components/FormSubmitButton";
 import TextField from "@mui/material/TextField";
 
@@ -7,39 +7,69 @@ class LoginForm extends React.Component<{}, any> {
 	constructor(props: any) {
 		super(props);
 		this.loginHandler = this.loginHandler.bind(this);
-		this.state = { username: "", password: "" };
+		this.state = { username: "", password: "", postId: Number };
 	}
 
 	//HTTP POST request to backend
 	loginHandler() {
-		window.alert(this.state.username + " " + this.state.password);
+		//TODO POST Request to backend
+		const requestOptions = {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				username: this.state.username,
+				password: this.state.password,
+			}),
+		};
+		fetch("http://localhost:3001/login", requestOptions)
+			.then(async (response) => {
+				const isJson = response.headers
+					.get("content-type")
+					?.includes("application/json");
+				const data = isJson && (await response.json());
+
+				// check for error response
+				if (!response.ok) {
+					// get error message from body or default to response status
+					const error = (data && data.message) || response.status;
+					return Promise.reject(error);
+				}
+
+				this.setState({ postId: data.id });
+			})
+			.catch((error) => {
+				this.setState({ errorMessage: error.toString() });
+				console.error("There was an error!", error);
+			});
 	}
 
 	render(): React.ReactNode {
 		return (
 			<div>
-				<div>
-					<TextField
-						type={"text"}
-						placeholder={"Username"}
-						value={this.state.username}
-						onChange={(e) => this.setState({ username: e.target.value })}
-						required
+				<form action="" method="post">
+					<div>
+						<TextField
+							type={"text"}
+							placeholder={"Username"}
+							value={this.state.username}
+							onChange={(e) => this.setState({ username: e.target.value })}
+							required
+						/>
+					</div>
+					<div>
+						<TextField
+							type={"password"}
+							placeholder={"Password"}
+							value={this.state.password}
+							onChange={(e) => this.setState({ password: e.target.value })}
+							required
+						/>
+					</div>
+					<FormSubmitButton
+						type={{ inputType: "Login" }}
+						onClickHandler={this.loginHandler}
 					/>
-				</div>
-				<div>
-					<TextField
-						type={"password"}
-						placeholder={"Password"}
-						value={this.state.password}
-						onChange={(e) => this.setState({ password: e.target.value })}
-						required
-					/>
-				</div>
-				<FormSubmitButton
-					type={{ inputType: "Login" }}
-					onClickHandler={this.loginHandler}
-				/>
+				</form>
 			</div>
 		);
 	}
