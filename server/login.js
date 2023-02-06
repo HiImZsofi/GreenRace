cce4666bd715ab02816b60f128e26af75e34b814
Gondi László <laszlo.zsolt.gondi@gmail.com>
2023-02-06 16:09:04 +0100

    Fix req.body not being parsed on the server

--- server/login.js
+++ server/login.js
@@ -1,14 +1,17 @@
 //Imports
 const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const cors = require("cors");
+var mysql = require("mysql2");
+const { connect } = require("http2");
+app.use(cors());
+const bp = require("body-parser");
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
 
 //Database config
 //? Maybe extract it to another js file for easier reuse
-const PORT = process.env.PORT || 3306;
-const app = express();
-const cors = require("cors");
-const { connect } = require("http2");
-app.use(cors());
-
 const connection = mysql.createConnection({
 	host: "localhost",
 	user: "root",
@@ -23,12 +26,12 @@ app.listen(PORT, () => {
 //User POSTs info to the backend
 app.post("/login", (req, res) => {
 	//Store data in from the POST request
-	const { username, password } = req.body;
+	//const { username, password } = req.body;
 	console.log(req.body, "Success");
 
-	let passwordInDB = connection.query(
-		'SELECT password FROM users WHERE username="asd"'
-	);
+	// let passwordInDB = connection.query(
+	// 	'SELECT password FROM users WHERE username="asd"'
+	// );
 
 	//Try username againts the database
 	// try {
@@ -45,10 +48,10 @@ app.post("/login", (req, res) => {
 	// }
 
 	//Check password againts the one fetched from the database
-	if (password === passwordInDB) {
-		//TODO: decrypt encrypted password
-		res.send("Successful login");
-	} else {
-		res.send("Incorrect password");
-	}
+	// if (password === passwordInDB) {
+	// 	//TODO: decrypt encrypted password
+	// 	res.send("Successful login");
+	// } else {
+	// 	res.send("Incorrect password");
+	// }
 });
