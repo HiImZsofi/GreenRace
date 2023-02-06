d4dd8a8be93d7acf929fa8913b0f4ac2b146b042
HiImZsofi <zsofia.gyori00@gmail.com>
2023-02-06 14:49:46 +0100

    insert user data into database

--- server/index.js
+++ server/index.js
@@ -1,17 +1,75 @@
-//require express.js
+//imports
 const express = require('express');
+var mysql = require('mysql2');
+var http = require('http');
+var bodyParser = require('body-parser');
+
+// create application/json parser
+var jsonParser = bodyParser.json();
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+//Connect to database
+const connection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  database: 'greenRace',
+  port: '3306'
+});
+
+//connect to database
+connection.connect((err) =>{
+  if(err) throw err;
+  console.log('Mysql Connected...');
+});
+
 
 //server port
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3001;
 const app = express();
-const cors = require("cors")
-app.use(cors())
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }))
+
+
+app.get("/", (req, res) => {
+  res.send("Main page");
+});
+
 
 //start server on given port
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
-app.get('/api', (request, response) => {
-    response.json({message: "Api ok"});
+app.post('/register', jsonParser, (req, res) => {
+  // let data = {
+  //   name: req.body.name,
+  //   passwords: req.body.password,
+  //   email: req.body.email
+  // };
+
+  var name = JSON.stringify(req.body.username); //TODO username buggy
+  var password = req.body.password;
+  var email = req.body.email;
+
+
+  // let sql = "INSERT INTO Users SET ?";
+  // let query = connection.query(sql, data,(err, results) => {
+  //   if(err) throw err;
+  //   res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  //   res.redirect('/');
+  // });
+
+  var sql = `INSERT INTO users (username, password, email) VALUES ("${name}", "${password}", "${email}")`; //TODO user id
+  connection.query(sql, function(err, result) {
+    if (err) throw err;
+    console.log('record inserted');
+    res.redirect('http://localhost:3001/');
+  });
 });
+
+//TODO ports are acting up 
