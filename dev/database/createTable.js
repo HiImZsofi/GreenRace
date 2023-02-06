d81e33d1599041f7e52d1e67c9c2c76d05296505
Benedek Ákos <benedek.akos.marcell@gmail.com>
2023-02-06 19:16:59 +0100

    Add AUTO_INCREMENT to every PRIMARY KEY

--- dev/database/createTable.js
+++ dev/database/createTable.js
@@ -17,54 +17,52 @@ connection.query(
 
 // Create users table
 connection.query(
-    'CREATE OR REPLACE TABLE Users (user_ID VARCHAR(255) PRIMARY KEY, username VARCHAR(30) NOT NULL, password VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, points INT, picfilename VARCHAR(255), picfilepath VARCHAR(255))',
+    'CREATE OR REPLACE TABLE Users (user_ID int PRIMARY KEY AUTO_INCREMENT, username VARCHAR(30) NOT NULL, password VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, points INT, picfilename VARCHAR(255), picfilepath VARCHAR(255))',
     function(err, result){
         if(err) throw err;
-            console.log("Table Users created or modified");          
+            console.log("Table Users created or modified");        
     }
 );
 
 // Create friendships table
 connection.query(
-    'CREATE OR REPLACE TABLE Friendships (friendship_ID VARCHAR(255) PRIMARY KEY, user_ID VARCHAR(255) NOT NULL, friend_id VARCHAR(255) NOT NULL, CONSTRAINT fk_fuuser_id FOREIGN KEY (user_ID) REFERENCES Users (user_ID), CONSTRAINT fk_ffuser_id FOREIGN KEY (friend_ID) REFERENCES Users (user_ID))',
+    'CREATE OR REPLACE TABLE Friendships (friendship_ID int PRIMARY KEY AUTO_INCREMENT, user_ID int NOT NULL, friend_id int NOT NULL, CONSTRAINT fk_fuuser_id FOREIGN KEY (user_ID) REFERENCES Users (user_ID), CONSTRAINT fk_ffuser_id FOREIGN KEY (friend_ID) REFERENCES Users (user_ID))',
     function(err, result){
         if(err) throw err;
-            console.log("Table Friendships created or modified");    
+            console.log("Table Friendships created or modified");   
     }
 );
 
 // Create achivements table
 connection.query(
-    'CREATE OR REPLACE TABLE Achivements (achivement_ID VARCHAR(255) PRIMARY KEY)',
+    'CREATE OR REPLACE TABLE Achivements (achivement_ID int PRIMARY KEY AUTO_INCREMENT)',
     function(err, result){
         if(err) throw err;
-            console.log("Table Achivements created or modified");    
+            console.log("Table Achivements created or modified");                
     }
 );
 
 // Create Completions table
 connection.query(
-    'CREATE OR REPLACE TABLE Completions (completion_ID VARCHAR(255) PRIMARY KEY, user_ID VARCHAR(255) NOT NULL, achivement_ID VARCHAR(255) NOT NULL, completion_date DATETIME NOT NULL, CONSTRAINT fk_cuser_id FOREIGN KEY (user_ID) REFERENCES Users (user_ID), CONSTRAINT fk_achi_id FOREIGN KEY (achivement_ID) REFERENCES Achivements (achivement_ID))',
+    'CREATE OR REPLACE TABLE Completions (completion_ID int PRIMARY KEY AUTO_INCREMENT, user_ID int NOT NULL, achivement_ID int NOT NULL, completion_date DATETIME NOT NULL, CONSTRAINT fk_cuser_id FOREIGN KEY (user_ID) REFERENCES Users (user_ID), CONSTRAINT fk_achi_id FOREIGN KEY (achivement_ID) REFERENCES Achivements (achivement_ID))',
     function(err, result){
         if(err) throw err;
-            console.log("Table Completions created or modified");
-               
+            console.log("Table Completions created or modified");             
     }
 );
 
 // Create Vehicles table
 connection.query(
-    'CREATE OR REPLACE TABLE Vehicles (vehicle_id VARCHAR(255) PRIMARY KEY, vehicle_type VARCHAR(30) NOT NULL, model VARCHAR(255), emission int NOT NULL)',
+    'CREATE OR REPLACE TABLE Vehicles (vehicle_id int PRIMARY KEY AUTO_INCREMENT, vehicle_type VARCHAR(30) NOT NULL, model VARCHAR(255), emission int NOT NULL)',
     function(err, result){
         if(err) throw err;
             console.log("Table Vehicles created or modified");
-               
     }
 );
 
 // Create Route table
 connection.query(
-    'CREATE OR REPLACE TABLE Routes (route_id VARCHAR(255) PRIMARY KEY, user_id VARCHAR(255) NOT NULL, vehicle_id VARCHAR(255) NOT NULL, length int, CONSTRAINT fk_ruser_id FOREIGN KEY (user_ID) REFERENCES Users (user_ID), CONSTRAINT fk_vehicle_id FOREIGN KEY (vehicle_id) REFERENCES Vehicles (vehicle_id))',
+    'CREATE OR REPLACE TABLE Routes (route_id int PRIMARY KEY AUTO_INCREMENT, user_id int NOT NULL, vehicle_id int NOT NULL, length int, CONSTRAINT fk_ruser_id FOREIGN KEY (user_ID) REFERENCES Users (user_ID), CONSTRAINT fk_vehicle_id FOREIGN KEY (vehicle_id) REFERENCES Vehicles (vehicle_id))',
     function(err, result){
         if(err) throw err;
             console.log("Table Routes created or modified");
