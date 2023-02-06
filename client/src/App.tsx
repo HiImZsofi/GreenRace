c840dd17fa2d59dd90db93f13820478d22ffe141
HiImZsofi <zsofia.gyori00@gmail.com>
2023-02-06 11:06:34 +0100

    use url based routing when rendering pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
 //imports
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { response } from 'express';
 import RegistrationFormHandler from './components/registrationFormHandler';
+import {  BrowserRouter as Router,  Routes,  Route,  Link, BrowserRouter  } from 'react-router-dom'; 
 
 function App() {
 
@@ -27,8 +28,14 @@ function App() {
 
 
     return (
-      <RegistrationFormHandler/>
-    )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<RegistrationFormHandler />}>
+          
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 
