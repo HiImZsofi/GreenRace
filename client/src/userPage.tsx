3458f0a561654ef34e208f4391eeaa6932d29ef0
Benedek Ákos <benedek.akos.marcell@gmail.com>
2023-02-07 14:27:42 +0100

    Navbar [in progress]

--- client/src/userPage.tsx
+++ client/src/userPage.tsx
@@ -1,22 +1,27 @@
 import React, { useEffect, useState } from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
+import Container from 'react-bootstrap/esm/Container';
 import './userPage.css';
 
 class UserPage extends React.Component<{}, any> {
 
     render(): React.ReactNode {
 		return (
-			<div>
-				<div>
-					<div id="menu">
-						<img id="logo" src = {"greenRaceLogo.png"} />
-  						<a href="#points">Pontjaim</a>
-  						<a href="#ranks">Rangsor</a>
-  						<a href="#friends">Barátok</a>
-						<img id="profpic" src = {"npic.png"}/>
-					</div>
-					
-				</div>
-			</div>
+			<>
+				<Navbar className='color-nav' bg='dark' variant='dark'>
+					<Container>
+						<Navbar.Brand>
+							<img src="/greenRaceLogo.png" width="30" height="30" className="d-inline-block align-top" alt="Green Race Logo"/>
+						</Navbar.Brand>
+						<Nav className="me-auto">
+        					<Nav.Link href="">Pontjaim</Nav.Link>
+           	 				<Nav.Link href="">Rangsor</Nav.Link>
+							<Nav.Link href="">Barátok</Nav.Link>
+          				</Nav>
+						<img src="/npic.png" width="30" height="30" className="d-inline-block align-top" alt="User Profile Pic"/>
+					</Container>			
+				</Navbar>
+			</>
 		);
     }
 }
