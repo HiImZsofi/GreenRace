1949b6220d61d568921e0eb9d2d53592bce5e6d1
Gondi László <laszlo.zsolt.gondi@gmail.com>
2023-02-07 14:23:37 +0100

    Edit Interfaces with additional optional values for InputTypeHandler interface

--- client/src/Interfaces.ts
+++ client/src/Interfaces.ts
@@ -1,8 +1,10 @@
-export interface UserLoginDto{
-    username: string,
-    password: string
+export interface UserLoginDto {
+	username: string;
+	password: string;
 }
 
-export interface InputTypeHandler{
-    inputType:string
+export interface InputTypeHandler {
+	inputType: string;
+	value?: string;
+	onChangeHandler?: (e: any) => void;
 }
