e67bc1e30b00c5afbf6f39efa152357916b42ba7
Gondi László <laszlo.zsolt.gondi@gmail.com>
2023-02-08 08:20:00 +0100

    Login page now uses /login as it's route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,32 @@
 //imports
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import { response } from 'express';
-import LoginForm from './Login';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginForm from "./Login";
 
 function App() {
+	const [data, setData] = useState(null);
+	const [error, setError] = useState(null);
 
-    const [data, setData] = useState(null);
-    const [error, setError] = useState(null);
-  
-    useEffect(() => {
-      fetch('http://localhost:3306/api')
-        .then(response => response.json())
-        .then(data => setData(data))
-        .catch(error => setError(error));
-    }, []);
-  
-    return (
-      <React.StrictMode>
-        <LoginForm />
-      </React.StrictMode>
-    );
+	useEffect(() => {
+		fetch("http://localhost:3306/api")
+			.then((response) => response.json())
+			.then((data) => setData(data))
+			.catch((error) => setError(error));
+	}, []);
+
+	const router = createBrowserRouter([
+		{
+			path: "/login",
+			element: <LoginForm />,
+		},
+	]);
+
+	return (
+		<React.StrictMode>
+			<RouterProvider router={router} />
+		</React.StrictMode>
+	);
 }
 
 
