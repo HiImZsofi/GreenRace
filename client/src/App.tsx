f3b660537f6a131424e2ab0f8fd3855e6020d221
Gondi László <laszlo.zsolt.gondi@gmail.com>
2023-02-16 10:41:37 +0100

    Clean up some unused imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,14 @@
 //imports
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+import React from "react";
+import "./App.css";
+import { Navigate, Routes, Route, BrowserRouter } from "react-router-dom";
 import Login from "./Views/Login";
-import Grid from "@mui/material/Grid";
-import { response } from "express";
-import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	Link,
-	BrowserRouter,
-} from "react-router-dom";
+import Register from "./Views/Register";
 import UserPage from "./Views/userPage";
 import RankPage from "./Views/rankPage";
 import FriendPage from "./Views/friendPage";
-import Register from "./Views/RegisterForm";
 
 function App() {
-	const [data, setData] = useState(null);
-	const [error, setError] = useState(null);
-
-	useEffect(() => {
-		fetch("http://localhost:3306/api")
-			.then((response) => response.json())
-			.then((data) => setData(data))
-			.catch((error) => setError(error));
-	}, []);
-	//Routes in the webapp
 	return (
 		//Routes in the webapp
 		<BrowserRouter>
