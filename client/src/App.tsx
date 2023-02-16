434145390241313859bd47ed26257f5816d07822
Gondi László <laszlo.zsolt.gondi@gmail.com>
2023-02-16 10:31:46 +0100

    Fix syntax error in view names

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
-import LoginForm from "./Views/Login";
+import Login from "./Views/Login";
 import Grid from "@mui/material/Grid";
 import { response } from "express";
 import {
@@ -15,7 +15,7 @@ import {
 import UserPage from "./Views/userPage";
 import RankPage from "./Views/rankPage";
 import FriendPage from "./Views/friendPage";
-import RegisterForm from "./Views/RegisterForm";
+import Register from "./Views/RegisterForm";
 
 function App() {
 	const [data, setData] = useState(null);
@@ -27,16 +27,16 @@ function App() {
 			.then((data) => setData(data))
 			.catch((error) => setError(error));
 	}, []);
-//Routes in the webapp
+	//Routes in the webapp
 	return (
 		//Routes in the webapp
 		<BrowserRouter>
 			<React.StrictMode>
 				<Routes>
-					<Route path="/" element={<Navigate to='/login'/>}/>
-					<Route path="/register" element={<RegisterForm />} />
-					<Route path="/login" element={<LoginForm />}></Route>
-					<Route path="/register" element={<RegisterForm />}></Route>
+					<Route path="/" element={<Navigate to="/login" />} />
+					<Route path="/register" element={<Register />} />
+					<Route path="/login" element={<Login />}></Route>
+					<Route path="/register" element={<Register />}></Route>
 					<Route path="/userPage" element={<UserPage />}></Route>
 					<Route path="/rankPage" element={<RankPage />}></Route>
 					<Route path="/friendPage" element={<FriendPage />}></Route>
