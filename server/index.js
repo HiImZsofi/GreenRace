b3d2d0d4c4792d37ccde316429770efe979fd1ed
Gondi László <laszlo.zsolt.gondi@gmail.com>
2023-02-16 09:53:03 +0100

    Remove finished todos

--- server/index.js
+++ server/index.js
@@ -100,13 +100,11 @@ app.post("/register", async (req, res) => {
 		await insertNewUser(sql);
 		res.statusCode = 200;
 		console.log("Inserted user", res.statusCode);
-    res.send(JSON.stringify({ result: "Successful registration" }));
+		res.send(JSON.stringify({ result: "Successful registration" }));
 	} catch (error) {
 		res.statusCode = 500;
 		console.log("Server error", res.statusCode);
 		res.json(JSON.stringify({ error: "Duplicate email" }));
 	}
 });
-//TODO send emails to validate registration
-//TODO ports are acting up
 //TODO SQL injection????
