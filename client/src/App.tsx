2867c37653dea36b99c0b507978118c8f2da2c3f
HiImZsofi <zsofia.gyori00@gmail.com>
2023-02-16 14:22:32 +0100

    remove old test fetch from app function

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,20 @@
 //imports
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 import LoginForm from "./Views/Login";
 import Grid from "@mui/material/Grid";
 import { response } from "express";
 import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	Link,
-	BrowserRouter,
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  BrowserRouter,
 } from "react-router-dom";
 import UserPage from "./Views/userPage";
 import RankPage from "./Views/rankPage";
@@ -18,32 +22,23 @@ import FriendPage from "./Views/friendPage";
 import RegisterForm from "./Views/RegisterForm";
 
 function App() {
-	const [data, setData] = useState(null);
-	const [error, setError] = useState(null);
-
-	useEffect(() => {
-		fetch("http://localhost:3306/api")
-			.then((response) => response.json())
-			.then((data) => setData(data))
-			.catch((error) => setError(error));
-	}, []);
-//Routes in the webapp
-	return (
-		//Routes in the webapp
-		<BrowserRouter>
-			<React.StrictMode>
-				<Routes>
-					<Route path="/" element={<Navigate to='/login'/>}/>
-					<Route path="/register" element={<RegisterForm />} />
-					<Route path="/login" element={<LoginForm />}></Route>
-					<Route path="/register" element={<RegisterForm />}></Route>
-					<Route path="/userPage" element={<UserPage />}></Route>
-					<Route path="/rankPage" element={<RankPage />}></Route>
-					<Route path="/friendPage" element={<FriendPage />}></Route>
-				</Routes>
-			</React.StrictMode>
-		</BrowserRouter>
-	);
+  //Routes in the webapp
+  return (
+    //Routes in the webapp
+    <BrowserRouter>
+      <React.StrictMode>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/login" element={<LoginForm />}></Route>
+          <Route path="/register" element={<RegisterForm />}></Route>
+          <Route path="/userPage" element={<UserPage />}></Route>
+          <Route path="/rankPage" element={<RankPage />}></Route>
+          <Route path="/friendPage" element={<FriendPage />}></Route>
+        </Routes>
+      </React.StrictMode>
+    </BrowserRouter>
+  );
 }
 
 export default App;
