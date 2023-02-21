c7a761e9125859009f07ba12eb40838455e48af9
Benedek Ákos <benedek.akos.marcell@gmail.com>
2023-02-21 15:44:48 +0100

    Fetch [InProgress]

--- client/src/Views/UserPage.tsx
+++ client/src/Views/UserPage.tsx
@@ -4,6 +4,7 @@ import NavMenu from "../components/NavBar";
 import 'bootstrap/dist/css/bootstrap.css';
 import { UserPageDto } from '../Interfaces';
 
+
 class UserPage extends React.Component<{}, UserPageDto> {
 	constructor(props: any) {
 		super(props);
@@ -15,13 +16,33 @@ class UserPage extends React.Component<{}, UserPageDto> {
 			points: 0,
 		};
 	}
+	dataLoadIn(){
+		const requestUserData = {
+			method:"GET",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				username: this.state.username,
+				picfilepath: this.state.picfilepath,
+				points: this.state.points,
+			}),
+		};
+		fetch("http://localhost:3001/userpage", requestUserData)
+		.then (async (response) => {
+			const isJson = response.headers
+			.get("content-type")
+			?.includes("application/json");
+			const data = isJson && (await response.json());
+			console.log(data);
+		})
+
+	};
     render(): React.ReactNode {
 		return (
 			<div key={"userPage"}>
 			<NavMenu username={this.state.username} picfilepath={this.state.picfilepath}/>
 				<div className='text-center mt-3'>
 					<div>
-						<h1>10000 <span id='pont'>Zöldpont</span>-od van</h1>
+						<h1>{this.state.points} <span id='pont'>Zöldpont</span>-od van</h1>
 						<p>Ez 1000 szenyezésnek felel meg</p>
 					</div>
 					<div>
@@ -38,3 +59,7 @@ class UserPage extends React.Component<{}, UserPageDto> {
     }
 }
 export default UserPage;
+
+function dataLoadIn() {
+	throw new Error('Function not implemented.');
+}
