cc27ae9f3c3848ef0e5df764d32f0e8059ff9152
László Gondi <laszlo.zsolt.gondi@gmail.com>
2023-02-24 18:31:20 +0100

    Remove todo

--- server/index.js
+++ server/index.js
@@ -259,7 +259,6 @@ app.post("/logout", (req, res) => {
 	res.send("Logged out");
 });
 
-//TODO destructure code
 app.post("/settings", async (req, res) => {
 	const { email, newUsername, newPassword, currentPassword } = req.body;
 	const passwordInDB = await getPassQuery(email).catch((error) => {
