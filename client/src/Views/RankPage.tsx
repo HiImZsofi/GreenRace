eb518a5ad7f1e73cc6363de554de61a86b9d8560
László Gondi <laszlo.zsolt.gondi@gmail.com>
2023-02-24 20:59:24 +0100

    Refactore from class to function component

--- client/src/Views/RankPage.tsx
+++ client/src/Views/RankPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Pages.css";
 import NavMenu from "../components/NavBar";
 import "bootstrap/dist/css/bootstrap.css";
@@ -9,21 +9,12 @@ interface Ranking {
 	points: number;
 }
 const Ranglist: Ranking[] = [];
-class RankPage extends React.Component<{}, UserPageDto> {
-	constructor(props: any) {
-		super(props);
-		this.logoutHandler = this.logoutHandler.bind(this);
-		//Initalize state variables
-		this.state = {
-			username: "",
-			picfilepath: "",
-			points: 0,
-			//This can be set to true because it should only be on pages when you are logged in
-			isLoggedIn: true,
-		};
-	}
-	//Navbar Logout-Button's Onclick Function
-	logoutHandler() {
+const RankPage = () => {
+	const [username, setUsername] = useState("");
+	const [picFilePath, setPicFilePath] = useState("");
+	const [points, setPoints] = useState("");
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const logoutHandler = () => {
 		const requestOptions = {
 			method: "POST",
 			headers: {
@@ -38,18 +29,15 @@ class RankPage extends React.Component<{}, UserPageDto> {
 					?.includes("application/json");
 				const data = isJson && (await response.json());
 				if (response.status == 200) {
-					this.setState({
-						isLoggedIn: false,
-					});
+					setIsLoggedIn(!isLoggedIn);
 				}
 			})
 
 			.catch((error) => {
 				console.error("There was an error!", error);
 			});
-	}
-	//Loading In The Users Data
-	loadInData() {
+	};
+	const loadInData = () => {
 		fetch("http://localhost:3001/userPage")
 			.then(async (response) => {
 				const isJson = response.headers
@@ -57,15 +45,15 @@ class RankPage extends React.Component<{}, UserPageDto> {
 					?.includes("application/json");
 				const data = isJson && (await response.json());
 				console.log(data);
-				this.setState({ username: data.userdata.username });
-				this.setState({ picfilepath: data.userdata.picfilepath });
-				this.setState({ points: data.userdata.points });
+				setUsername(data.userdata.username);
+				setPicFilePath(data.userdata.picfilepath);
+				setPoints(data.userdata.points);
 			})
 			.catch((error) => {
 				console.error("There was an error!", error);
 			});
-	}
-	loadInRankData() {
+	};
+	const loadInRankData = () => {
 		fetch("http://localhost:3001/rankPage")
 			.then(async (response) => {
 				const isJson = response.headers
@@ -85,38 +73,36 @@ class RankPage extends React.Component<{}, UserPageDto> {
 			.catch((error) => {
 				console.error("There was an error!", error);
 			});
-	}
-	componentDidMount() {
-		this.loadInData();
-		this.loadInRankData();
-	}
-	//Rendering Page
-	render(): React.ReactNode {
-		if (!this.state.isLoggedIn) {
-			return <Navigate to="/login" replace={true} />;
-		} else {
-			return (
-				<div key={"rankPage"}>
-					<NavMenu
-						username={this.state.username}
-						picfilepath={this.state.picfilepath}
-						logoutHandler={this.logoutHandler}
-					/>
-					<div className="text-center mt-3">
-						<div>
-							<h1>Rang Lista:</h1>
-							<ul>
-								{Ranglist.map((Ranking, i) => (
-									<li key={i}>
-										{i + 1}. {Ranking.username}:{Ranking.points}p
-									</li>
-								))}
-							</ul>
-						</div>
+	};
+	useEffect(() => {
+		loadInData();
+		loadInRankData();
+	});
+	if (isLoggedIn) {
+		return <Navigate to="/login" replace={true} />;
+	} else {
+		return (
+			<div key={"rankPage"}>
+				<NavMenu
+					username={username}
+					picfilepath={picFilePath}
+					logoutHandler={logoutHandler}
+				/>
+				<div className="text-center mt-3">
+					<div>
+						<h1>Rang Lista:</h1>
+						<ul>
+							{Ranglist.map((Ranking, i) => (
+								<li key={i}>
+									{i + 1}. {Ranking.username}:{Ranking.points}p
+								</li>
+							))}
+						</ul>
 					</div>
 				</div>
-			);
-		}
+			</div>
+		);
 	}
-}
+};
+
 export default RankPage;
