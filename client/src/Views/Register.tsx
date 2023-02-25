fd81bc377d934fd15eb3d7627f2bf467072be391
László Gondi <laszlo.zsolt.gondi@gmail.com>
2023-02-25 14:13:35 +0100

    Refactor class component to function component

--- client/src/Views/Register.tsx
+++ client/src/Views/Register.tsx
@@ -1,72 +1,59 @@
-import Box from "@mui/material/Box";
-import React from "react";
-import { UserRegisterDto } from "../Interfaces";
+//Imports
+import React, { useState } from "react";
 import FormSubmitButton from "../components/FormSubmitButton";
 import InputField from "../components/InputField";
-import Card from "@mui/material/Card";
-import Grid from "@mui/material/Grid";
-import { Navigate, redirect, useNavigate } from "react-router-dom";
-import { response } from "express";
+import { useNavigate } from "react-router-dom";
 import FormWrapper from "../components/FormWrapper";
 import FormRedirectLink from "../components/FormRedirectLink";
 
-class Register extends React.Component<{}, UserRegisterDto> {
-	constructor(props: any) {
-		super(props);
-		this.usernameChangeHandler = this.usernameChangeHandler.bind(this);
-		this.passwordChangeHandler = this.passwordChangeHandler.bind(this);
-		this.emailChangeHandler = this.emailChangeHandler.bind(this);
-		this.submitHandler = this.submitHandler.bind(this);
+const Register = () => {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [email, setEmail] = useState("");
+	const [usernameErr, setUsernameErr] = useState(false);
+	const [passwordErr, setPasswordErr] = useState(false);
+	const [emailErr, setEmailErr] = useState(false);
+	const [usernameErrMsg, setUsernameErrMsg] = useState("");
+	const [passwordErrMsg, setPasswordErrMsg] = useState("");
+	const [emailErrMsg, setEmailErrMsg] = useState("");
 
-		this.state = {
-			username: "",
-			password: "",
-			email: "",
-			usernameErr: false,
-			passwordErr: false,
-			emailErr: false,
-			usernameErrMsg: "",
-			passwordErrMsg: "",
-			emailErrMsg: "",
-			registerSuccess: false,
-		};
-	}
+	const navigate = useNavigate();
 
-	usernameChangeHandler(e: React.SyntheticEvent<HTMLInputElement>) {
-		this.setState({ username: e.currentTarget.value });
-	}
+	const usernameChangeHandler = (e: React.SyntheticEvent<HTMLInputElement>) => {
+		setUsername(e.currentTarget.value);
+	};
 
-	passwordChangeHandler(e: React.SyntheticEvent<HTMLInputElement>) {
-		this.setState({ password: e.currentTarget.value });
-	}
+	const passwordChangeHandler = (e: React.SyntheticEvent<HTMLInputElement>) => {
+		setPassword(e.currentTarget.value);
+	};
 
-	emailChangeHandler(e: React.SyntheticEvent<HTMLInputElement>) {
-		this.setState({ email: e.currentTarget.value });
-	}
+	const emailChangeHandler = (e: React.SyntheticEvent<HTMLInputElement>) => {
+		setEmail(e.currentTarget.value);
+	};
 
-	submitHandler() {
+	const submitHandler = () => {
 		if (
-			this.state.username.trim() == null ||
-			this.state.username.trim() == "" ||
-			this.state.email.trim() == null ||
-			this.state.email.trim() == "" ||
-			this.state.password.trim() == null ||
-			this.state.password.trim() == ""
+			username.trim() == null ||
+			username.trim() == "" ||
+			email.trim() == null ||
+			email.trim() == "" ||
+			password.trim() == null ||
+			password.trim() == ""
 		) {
-			this.setState({ emailErr: true });
-			this.setState({ passwordErr: true });
-			this.setState({
-				emailErrMsg: "Invalid input",
-				passwordErrMsg: "Invalid input",
-			});
+			setEmailErr(true);
+			setUsernameErr(true);
+			setPasswordErr(true);
+			setEmailErrMsg("Invalid input");
+			setUsernameErrMsg("Invalid input");
+			setPasswordErrMsg("Invalid input");
 		} else {
 			const requestOptions = {
 				method: "POST",
 				headers: { "Content-Type": "application/json" },
 				body: JSON.stringify({
-					username: this.state.username,
-					password: this.state.password,
-					email: this.state.email,
+					username: username,
+					password: password,
+					email: email,
 				}),
 			};
 			fetch("http://localhost:3001/register", requestOptions)
@@ -77,13 +64,10 @@ class Register extends React.Component<{}, UserRegisterDto> {
 					const data = isJson && (await response.json()); //if response headers include json then await
 					//Check for server response
 					if (response.status == 200) {
-						//TODO reroute to main page
-						this.setState({ registerSuccess: true });
+						navigate("/login", { replace: true });
 					} else if (response.status == 500) {
-						this.setState({
-							emailErr: true,
-							emailErrMsg: "Email already in use",
-						});
+						setEmailErr(true);
+						setEmailErrMsg("Email already in use");
 					} else {
 						// get error message from body or default to response status
 						const error = (data && data.message) || response.status;
@@ -94,57 +78,47 @@ class Register extends React.Component<{}, UserRegisterDto> {
 					console.error("There was an error!", error);
 				});
 		}
-	}
+	};
 
-	render(): React.ReactNode {
-		if (this.state.registerSuccess) {
-			return <Navigate to="/login" replace={true} />;
-		} else {
-			return (
-				<FormWrapper vhnum="100vh">
-					<InputField
-						type={{
-							inputType: "Username",
-							placeholder: "Username",
-							value: this.state.username,
-							onChangeHandler: this.usernameChangeHandler,
-						}}
-						error={this.state.usernameErr}
-						errorMessage={this.state.usernameErrMsg}
-					/>
-					<InputField
-						type={{
-							inputType: "Password",
-							placeholder: "Password",
-							value: this.state.password,
-							onChangeHandler: this.passwordChangeHandler,
-						}}
-						error={this.state.passwordErr}
-						errorMessage={this.state.passwordErrMsg}
-					/>
-					<InputField
-						type={{
-							inputType: "Email",
-							placeholder: "Email",
-							value: this.state.email,
-							onChangeHandler: this.emailChangeHandler,
-						}}
-						error={this.state.emailErr}
-						errorMessage={this.state.emailErrMsg}
-					/>
-					<FormSubmitButton
-						type={{ inputType: "Register" }}
-						onClickHandler={this.submitHandler}
-					/>
-					<FormRedirectLink
-						url="/login"
-						classname="LRlink"
-						text="Már van fiókom"
-					/>
-				</FormWrapper>
-			);
-		}
-	}
-}
+	return (
+		<FormWrapper vhnum="100vh">
+			<InputField
+				type={{
+					inputType: "Username",
+					placeholder: "Username",
+					value: username,
+					onChangeHandler: usernameChangeHandler,
+				}}
+				error={usernameErr}
+				errorMessage={usernameErrMsg}
+			/>
+			<InputField
+				type={{
+					inputType: "Password",
+					placeholder: "Password",
+					value: password,
+					onChangeHandler: passwordChangeHandler,
+				}}
+				error={passwordErr}
+				errorMessage={passwordErrMsg}
+			/>
+			<InputField
+				type={{
+					inputType: "Email",
+					placeholder: "Email",
+					value: email,
+					onChangeHandler: emailChangeHandler,
+				}}
+				error={emailErr}
+				errorMessage={emailErrMsg}
+			/>
+			<FormSubmitButton
+				type={{ inputType: "Register" }}
+				onClickHandler={submitHandler}
+			/>
+			<FormRedirectLink url="/login" classname="LRlink" text="Már van fiókom" />
+		</FormWrapper>
+	);
+};
 
 export default Register;
