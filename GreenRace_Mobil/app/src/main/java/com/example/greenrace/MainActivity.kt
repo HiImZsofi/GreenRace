8ac5ddbf8ea2fa9c5fb8900bad982400689f9434
HiImZsofi <zsofia.gyori00@gmail.com>
2023-03-03 12:06:23 +0100

    add intent to button which navigates to registry page

--- GreenRace_Mobil/app/src/main/java/com/example/greenrace/MainActivity.kt
+++ GreenRace_Mobil/app/src/main/java/com/example/greenrace/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.greenrace
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var registerButton : Button;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+
+        registerButton.setOnClickListener(){
+            navigateToRegistry()
+        }
+
+    }
+    fun navigateToRegistry(){
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
+    }
+
+
+    fun init() {
+        registerButton = findViewById(R.id.registerButton)
     }
 }
