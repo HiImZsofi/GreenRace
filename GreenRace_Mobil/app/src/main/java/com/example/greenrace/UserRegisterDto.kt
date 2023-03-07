ab91a460cb92f478059d954e3d89ba11fd437730
HiImZsofi <zsofia.gyori00@gmail.com>
2023-03-07 12:25:15 +0100

    User info + Response model data class for requests

--- GreenRace_Mobil/app/src/main/java/com/example/greenrace/UserRegisterDto.kt
+++ GreenRace_Mobil/app/src/main/java/com/example/greenrace/UserRegisterDto.kt
@@ -1,5 +1,18 @@
 package com.example.greenrace
 
 import android.provider.ContactsContract.CommonDataKinds.Email
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder
+import com.google.gson.annotations.SerializedName
 
-data class UserRegisterDto(val name: String, val password: String, val email: String)
+data class RequestModel(
+    @JsonProperty("username") val username: String,
+
+    @JsonProperty("password") val password: String,
+
+    @JsonProperty("email") val email: String
+    )
+
+data class ResponseModel(
+    val message: String
+)
