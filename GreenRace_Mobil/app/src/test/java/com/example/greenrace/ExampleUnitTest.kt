69d6b15541921ba5bd59f3028fb878137fcaa73e
HiImZsofi <zsofia.gyori00@gmail.com>
2023-03-07 14:16:55 +0100

    Unit test for object deserialization exceÅption

--- GreenRace_Mobil/app/src/test/java/com/example/greenrace/ExampleUnitTest.kt
+++ GreenRace_Mobil/app/src/test/java/com/example/greenrace/ExampleUnitTest.kt
@@ -1,8 +1,11 @@
 package com.example.greenrace
 
-import org.junit.Test
-
+import androidx.core.app.Person
+import com.fasterxml.jackson.databind.JsonMappingException
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.Assert.*
+import org.junit.Test
+import java.io.IOException
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +14,26 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
+    /*fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }*/
+
+    @Throws(IOException::class)
+    fun givenJsonObject_whenDeserializingIntoString_thenException() {
+        val json =
+            "{\"firstName\":\"Azhrioun\",\"lastName\":\"Abderrahim\",\"contact\":{\"email\":\"azh@email.com\"}}"
+        val mapper = ObjectMapper()
+        val exception: Exception = assertThrows(JsonMappingException::class.java) {
+            mapper.reader()
+                .forType(Person::class.java)
+                .readValue(json)
+        }
+        exception.message?.let {
+            assertTrue(
+                it
+                    .contains("Cannot deserialize value of type `java.lang.String` from Object value (token `JsonToken.START_OBJECT`)")
+            )
+        }
     }
+
 }
