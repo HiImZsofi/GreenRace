932ba5f178f10fb165815dffbc7e769eb836bd20
HiImZsofi <zsofia.gyori00@gmail.com>
2023-03-07 12:26:42 +0100

    Create intent to navigate to registry page

--- GreenRace_Mobil/app/src/main/java/com/example/greenrace/MainActivity.kt
+++ GreenRace_Mobil/app/src/main/java/com/example/greenrace/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.greenrace
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
