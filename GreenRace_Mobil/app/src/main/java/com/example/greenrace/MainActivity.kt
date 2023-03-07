d6415faafa8dccb415a560d04c47ca5a2a9fe859
Gondi László <laszlo.zsolt.gondi@gmail.com>
2023-03-07 13:17:24 +0100

    Add redirect to MapsActivity

--- GreenRace_Mobil/app/src/main/java/com/example/greenrace/MainActivity.kt
+++ GreenRace_Mobil/app/src/main/java/com/example/greenrace/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.greenrace
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,7 +10,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val intent = Intent(this@MainActivity, MapsActivity::class.java)
         startActivity(intent)
-        finish()
 
     }
 }
