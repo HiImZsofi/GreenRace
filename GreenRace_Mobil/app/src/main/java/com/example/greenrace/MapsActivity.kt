6c59960ea461b5267d192dd3b5e7997097e4a54a
Gondi László <laszlo.zsolt.gondi@gmail.com>
2023-03-07 13:53:06 +0100

    Add google maps to MapsActivity

--- GreenRace_Mobil/app/src/main/java/com/example/greenrace/MapsActivity.kt
+++ GreenRace_Mobil/app/src/main/java/com/example/greenrace/MapsActivity.kt
@@ -1,7 +1,16 @@
 package com.example.greenrace
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -9,30 +18,51 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+internal class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var mLocationPermissionGranted = false
 
     private lateinit var mMap: GoogleMap
 
-    //TODO Fix null pointer exception
+    private lateinit var latitudeTextView:TextView
+    private lateinit var longitudeTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        latitudeTextView=findViewById(R.id.latitude)
+        longitudeTextView=findViewById(R.id.longitude)
+
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        getLocationPermission()
+
+        try {
+
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location : Location? ->
+                    // Got last known location. In some rare situations this can be null.
+                    if(location!=null){
+                        latitudeTextView.text= location.latitude.toString()
+                        longitudeTextView.text= location.longitude.toString()
+                    }else{
+                        latitudeTextView.text="Error"
+                        longitudeTextView.text="Error"
+                    }
+                }
+        } catch (e: SecurityException) {
+            // Handle exception
+            e.message?.let { Log.e("Security_Exception", it) }
+        }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
@@ -43,6 +73,37 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             .title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
+
+    private fun getLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this.applicationContext,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true
+        } else {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+            //ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_NETWORK_STATE),)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+                                            grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        mLocationPermissionGranted = false
+        when (requestCode) {
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true
+                }
+            }
+        }
+    }
+
+
+
+
+
 }
 
       
