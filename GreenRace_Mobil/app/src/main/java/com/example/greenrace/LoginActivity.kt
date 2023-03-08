82e1d698bc8e2e1949cf62825f68646386f038dc
HiImZsofi <zsofia.gyori00@gmail.com>
2023-03-08 10:50:10 +0100

    Login http request algorithm

--- GreenRace_Mobil/app/src/main/java/com/example/greenrace/LoginActivity.kt
+++ GreenRace_Mobil/app/src/main/java/com/example/greenrace/LoginActivity.kt
@@ -1,9 +1,13 @@
 package com.example.greenrace
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LoginActivity : AppCompatActivity() {
 
@@ -14,6 +18,34 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         init()
+
+        val response = ServiceBuilder.buildService(ApiInterface::class.java)
+
+        loginConfirmButton.setOnClickListener(){
+            val requestModelLogin = RequestModelLogin(loginEmail.text.toString(), loginPassword.text.toString())
+
+            response.sendReq(requestModelLogin).enqueue(
+                object : Callback<ResponseModel> {
+                    override fun onResponse(
+                        call: Call<ResponseModel>,
+                        response: Response<ResponseModel>
+                    ) {
+                        if(response.code() == 401){
+                            loginEmail.setBackgroundResource(R.drawable.email_error)
+                            loginPassword.setBackgroundResource(R.drawable.email_error)
+                            loginEmail.error = "Az e-mail cím vagy a jelszó nem egyezik!"
+                            loginPassword.error = "Az e-mail cím vagy a jelszó nem egyezik!"
+                        } else {
+                            loginEmail.setBackgroundResource(R.drawable.email_normal)
+                            loginPassword.setBackgroundResource(R.drawable.email_normal)
+                        }
+                    }
+                    override fun onFailure(call: Call<ResponseModel>, t: Throwable) {
+                        Log.i("Error", t.toString())
+                    }
+                }
+            )
+        }
     }
 
     fun init(){
