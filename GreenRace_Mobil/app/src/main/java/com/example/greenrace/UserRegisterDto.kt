6bab4a8a4a19b64c0f3ac16fd26c07b8f417e89f
HiImZsofi <zsofia.gyori00@gmail.com>
2023-03-08 10:48:26 +0100

    Data class for login

--- GreenRace_Mobil/app/src/main/java/com/example/greenrace/UserRegisterDto.kt
+++ GreenRace_Mobil/app/src/main/java/com/example/greenrace/UserRegisterDto.kt
@@ -3,12 +3,18 @@ package com.example.greenrace
 import com.fasterxml.jackson.annotation.JsonProperty
 
 //user info data model
-data class RequestModel(
+data class RequestModelRegistration(
     @JsonProperty("username") val username: String = "",
 
     @JsonProperty("password") val password: String = "",
 
     @JsonProperty("email") val email: String = ""
+)
+
+data class RequestModelLogin(
+    @JsonProperty("email") val email: String = "",
+
+    @JsonProperty("password") val password: String = ""
 )
 
 //http response model
