bead231cb9add24ce997f50c3b5018b8e092cdb3
HiImZsofi <zsofia.gyori00@gmail.com>
2023-03-09 14:37:09 +0100

    Fixed mismatched type exception

--- GreenRace_Mobil/app/src/main/java/com/example/greenrace/RegisterActivity.kt
+++ GreenRace_Mobil/app/src/main/java/com/example/greenrace/RegisterActivity.kt
@@ -2,7 +2,9 @@ package com.example.greenrace
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
+import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -12,12 +14,13 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+
 class RegisterActivity : AppCompatActivity() {
 
-    private lateinit var registrationEmail : EditText
-    private lateinit var registrationPassword : EditText
-    private lateinit var registrationUsername : EditText
-    private lateinit var registrationConfirmButton : Button
+    private lateinit var registrationEmail: EditText
+    private lateinit var registrationPassword: EditText
+    private lateinit var registrationUsername: EditText
+    private lateinit var registrationConfirmButton: Button
     private lateinit var regToLoginText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,46 +34,74 @@ class RegisterActivity : AppCompatActivity() {
         registrationConfirmButton.setOnClickListener {
 
             //putting user info into the request model class
-            val requestModelRegistration = RequestModelRegistration(registrationUsername.text.toString(), registrationPassword.text.toString(), registrationEmail.text.toString())
+            val requestModelRegistration = RequestModelRegistration(
+                registrationUsername.text.toString(),
+                registrationPassword.text.toString(),
+                registrationEmail.text.toString()
+            )
 
-            try{
+            if (isValidEmail(registrationEmail.text.toString()) == false) {
+                registrationEmail.setBackgroundResource(R.drawable.email_error)
+                registrationEmail.error = "Az e-mail cím nem megfelelő formátumú!"
+            } else {
+                registrationEmail.setBackgroundResource(R.drawable.email_normal)
+                registrationPassword.setBackgroundResource(R.drawable.email_normal)
+                registrationUsername.setBackgroundResource(R.drawable.email_normal)
+
                 response.sendReq(requestModelRegistration).enqueue(
                     object : Callback<ResponseModel> {
-
                         //callback for a response
                         override fun onResponse(
                             call: Call<ResponseModel>,
                             response: Response<ResponseModel>
                         ) {
                             //if email is already taken catch 500 response code
-                            if(response.code() == 500){
+                            if (response.code() == 500) {
                                 //change background resource to red
                                 registrationEmail.setBackgroundResource(R.drawable.email_error)
                                 registrationEmail.error = "Ez az e-mail cím már foglalt!"
-                            } else{
+                            } /*else if(response.code() == 200) {
+                                    startActivity(regSuccessful)
+                                    finish()
+                                }*/ else {
+                                val regSuccessful =
+                                    Intent(this@RegisterActivity, LoginActivity::class.java)
+
+                                startActivity(regSuccessful)
+                                finish()
                                 registrationEmail.setBackgroundResource(R.drawable.email_normal)
+                                registrationPassword.setBackgroundResource(R.drawable.email_normal)
+                                registrationUsername.setBackgroundResource(R.drawable.email_normal)
                             }
                         }
+
                         //response failure call
                         override fun onFailure(call: Call<ResponseModel>, t: Throwable) {
+                            /*startActivity(regSuccessful)
+                                finish()
+                                registrationEmail.setBackgroundResource(R.drawable.email_normal)
+                                registrationPassword.setBackgroundResource(R.drawable.email_normal)
+                                registrationUsername.setBackgroundResource(R.drawable.email_normal)*/
                             Log.i("Error", t.toString())
                         }
                     }
-                )
-            }catch (e: MismatchedInputException){//TODO correct this error
-                Log.e("Error", e.message.toString())
-            }
 
+                )
+            }
         }
 
-        regToLoginText.setOnClickListener(){
+        regToLoginText.setOnClickListener() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }
     }
 
-    fun init(){
+    fun isValidEmail(target: CharSequence?): Boolean {
+        return !TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches()
+    }
+
+    fun init() {
         registrationEmail = findViewById(R.id.registrationEmail)
         registrationPassword = findViewById(R.id.registrationPassword)
         registrationConfirmButton = findViewById(R.id.registrationConfirmButton)
--- GreenRace_Mobil/app/src/main/java/com/example/greenrace/UserRegisterDto.kt
+++ GreenRace_Mobil/app/src/main/java/com/example/greenrace/UserRegisterDto.kt
@@ -1,23 +1,27 @@
 package com.example.greenrace
 
+import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
 
 //user info data model
-data class RequestModelRegistration(
-    @JsonProperty("username") val username: String = "",
 
-    @JsonProperty("password") val password: String = "",
+class RequestModelRegistration(username:String, password:String, email:String) {
+    @get:JsonCreator
+    @JsonProperty("username") val username: String = username
 
-    @JsonProperty("email") val email: String = ""
-)
+    @JsonProperty("password") val password: String = password
 
-data class RequestModelLogin(
-    @JsonProperty("email") val email: String = "",
+    @JsonProperty("email") val email: String = email
+}
 
-    @JsonProperty("password") val password: String = ""
-)
+class RequestModelLogin(email: String,password: String){
+    @get:JsonCreator
+    @JsonProperty("email") val email: String = email
+
+    @JsonProperty("password") val password: String = password
+}
 
 //http response model
 data class ResponseModel(
-    val message: String
+    @JsonProperty("result") val message: String?
 )
