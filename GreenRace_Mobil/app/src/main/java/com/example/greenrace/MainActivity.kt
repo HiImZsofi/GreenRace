ec26ad9b42de4c7a802f04179638e9ee4d9adb3f
László Gondi <laszlo.zsolt.gondi@gmail.com>
2023-03-09 15:00:15 +0100

    Revert "Mobile registry"

--- GreenRace_Mobil/app/src/main/java/com/example/greenrace/MainActivity.kt
+++ GreenRace_Mobil/app/src/main/java/com/example/greenrace/MainActivity.kt
@@ -1,42 +1,15 @@
 package com.example.greenrace
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var registerButton : Button
-    private lateinit var loginButton : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        init()
-
-        registerButton.setOnClickListener {
-            navigateToRegistry()
-        }
-
-        loginButton.setOnClickListener {
-            navigateToLogin()
-        }
-
-    }
-    //create intent for navigation
-    fun navigateToRegistry(){
-        val intent = Intent(this, RegisterActivity::class.java)
+        val intent = Intent(this@MainActivity, MapsActivity::class.java)
         startActivity(intent)
-        finish()
-    }
-    fun navigateToLogin(){
-        val intent = Intent(this, LoginActivity::class.java)
-        startActivity(intent)
-        finish()
-    }
 
-
-    fun init() {
-        registerButton = findViewById(R.id.registerButton)
-        loginButton = findViewById(R.id.loginButton)
     }
 }
--- GreenRace_Mobil/app/src/test/java/com/example/greenrace/ExampleUnitTest.kt
+++ GreenRace_Mobil/app/src/test/java/com/example/greenrace/ExampleUnitTest.kt
@@ -1,11 +1,8 @@
 package com.example.greenrace
 
-import androidx.core.app.Person
-import com.fasterxml.jackson.databind.JsonMappingException
-import com.fasterxml.jackson.databind.ObjectMapper
-import org.junit.Assert.*
 import org.junit.Test
-import java.io.IOException
+
+import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -14,21 +11,7 @@ import java.io.IOException
  */
 class ExampleUnitTest {
     @Test
-    /*fun addition_isCorrect() {
+    fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
-    }*/
-
-    @Throws(IOException::class)
-    fun givenJsonObject_whenDeserializingIntoString_thenException() {
-        val json =
-            "{\"username\":\"Azhrioun\",\"email\":\"Abderrahim@gmail.com\",\"password\":\"yxcasd\"}}"
-        val mapper = ObjectMapper()
-        val exception: Exception = assertThrows(JsonMappingException::class.java) {
-            mapper.reader()
-                .forType(Person::class.java)
-                .readValue(json)
-        }
-        exception.message?.contains("Cannot construct instance of `com.example.greenrace.ResponseModel` (although at least one Creator exists): cannot deserialize from Object value (no delegate- or property-based Creator)")
-        }
     }
-
+}
