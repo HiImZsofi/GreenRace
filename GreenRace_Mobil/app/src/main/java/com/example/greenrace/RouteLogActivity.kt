bb4ca870c586f7bbb951f5b9fd72f8305076e5ac
Gondi László <laszlo.zsolt.gondi@gmail.com>
2023-03-10 16:29:31 +0100

    Add spinner adapter placeholder

--- GreenRace_Mobil/app/src/main/java/com/example/greenrace/RouteLogActivity.kt
+++ GreenRace_Mobil/app/src/main/java/com/example/greenrace/RouteLogActivity.kt
@@ -1,9 +1,11 @@
 package com.example.greenrace
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+
 
 class RouteLogActivity : AppCompatActivity() {
     private lateinit var vehicleTypeSpinner: Spinner
@@ -19,6 +21,7 @@ class RouteLogActivity : AppCompatActivity() {
 
         initElements()
 
+
     }
 
     private fun initElements(){
@@ -27,5 +30,19 @@ class RouteLogActivity : AppCompatActivity() {
         getOnStopSpinner=findViewById(R.id.getOnStopSpinner)
         getOffStopSpinner=findViewById(R.id.getOffStopSpinner)
         logRouteButton=findViewById(R.id.logRouteButton)
+    }
+
+    private fun getLineRouteData(){
+        // Array of choices
+        val colors =
+            arrayOf("Red", "Blue", "White", "Yellow", "Black", "Green", "Purple", "Orange", "Grey")
+        // Application of the Array to the Spinner
+        val spinnerArrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, colors)
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item) // The drop down view
+
+        vehicleTypeSpinner.adapter = spinnerArrayAdapter
+
+        //TODO GET request to the backend
+
     }
 }
