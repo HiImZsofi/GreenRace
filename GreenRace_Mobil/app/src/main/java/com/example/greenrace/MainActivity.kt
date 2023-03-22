b2ab9ed9f3ac63036002ac0b2ab7f0b0ace0faf0
Benedek Ákos <benedek.akos.marcell@gmail.com>
2023-03-22 10:39:23 +0100

    Add working nav drawer for Mainpages

--- GreenRace_Mobil/app/src/main/java/com/example/greenrace/MainActivity.kt
+++ GreenRace_Mobil/app/src/main/java/com/example/greenrace/MainActivity.kt
@@ -1,31 +1,64 @@
+// Imports
 package com.example.greenrace
 
+import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
-import com.example.greenrace.swipeTouchListener.OnSwipeTouchListener
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import com.example.greenrace.sharedPreferences.PageNum
+import com.example.greenrace.swipeTouchListener.OnSwipeTouchListener
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var myView : LinearLayout
-    private lateinit var pageName : TextView
-    var menulist: List<String> = listOf("UserPage", "RankPage", "FriendPage")
-    var pagenumber: Int = 0
-    var fragmentlist: List<Fragment> = listOf(UserPageFragment(),RankPageFragment(),FriendPageFragment())
+    // Declare variables and views
+    private lateinit var myView: LinearLayout
+    private lateinit var pageName: TextView
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var buttonOpenDrawer: ImageButton
+
+    private var menulist: List<String> = listOf("UserPage", "RankPage", "FriendPage")
+    private var pagenumber: Int = 0
+    private var fragmentlist: List<Fragment> = listOf(UserPageFragment(), RankPageFragment(), FriendPageFragment())
+    // Override the onCreate method to initialize views and set up the layout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         init()
+        setUpDrawerLayout()
+        setUpSwipeListener()
         setter()
-        // Set up swipe listener on your view
+    }
+    // Set the page name and replace the fragment
+    private fun setter() {
+        pageName.text = menulist[pagenumber]
+        replaceFragment(R.id.fragmentContainerView, fragmentlist[pagenumber])
+    }
+    // Replace the current fragment with the fragment at the given frame ID
+    private inline fun FragmentManager.doTransaction(func: FragmentTransaction.() -> FragmentTransaction) {
+        beginTransaction().func().commit()
+    }
+
+    private fun AppCompatActivity.replaceFragment(frameId: Int, fragment: Fragment) {
+        supportFragmentManager.doTransaction { replace(frameId, fragment) }
+    }
+    // Set up the swipe listener for the view
+    private fun setUpSwipeListener() {
         val swipeListener = object : OnSwipeTouchListener(this@MainActivity) {
             override fun onSwipeLeft() {
-                if(pagenumber<menulist.size-1) { pagenumber++ } else { pagenumber = 0 }
+                if (pagenumber < menulist.size - 1) {
+                    pagenumber++
+                } else {
+                    pagenumber = 0
+                }
                 val pageNum = PageNum(this@MainActivity)
                 pageNum.savePageNum(pagenumber)
                 val intent = Intent(this@MainActivity, MainActivity::class.java)
@@ -35,7 +68,11 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onSwipeRight() {
-                if(pagenumber > 0) { pagenumber-- } else { pagenumber = menulist.size-1 }
+                if (pagenumber > 0) {
+                    pagenumber--
+                } else {
+                    pagenumber = menulist.size - 1
+                }
                 val pageNum = PageNum(this@MainActivity)
                 pageNum.savePageNum(pagenumber)
                 val intent = Intent(this@MainActivity, MainActivity::class.java)
@@ -46,20 +83,33 @@ class MainActivity : AppCompatActivity() {
         }
         myView.setOnTouchListener(swipeListener)
     }
-    inline fun FragmentManager.doTransaction(func: FragmentTransaction.() ->
-    FragmentTransaction) {
-        beginTransaction().func().commit()
+
+    // Set up the drawer layout, including open and close drawer methods
+    private fun setUpDrawerLayout() {
+        buttonOpenDrawer.setOnClickListener {
+            openCloseDrawer(buttonOpenDrawer)
+        }
+        val openDialog = Dialog(this)
+        openDialog.setContentView(R.layout.nav_header)
+        val closeButton = openDialog.findViewById<ImageButton>(R.id.close_button)
+        closeButton.setOnClickListener {
+            drawerLayout.closeDrawer(GravityCompat.END)
+        }
     }
-    fun AppCompatActivity.replaceFragment(frameId: Int, fragment: Fragment) {
-        supportFragmentManager.doTransaction{replace(frameId, fragment)}
+    fun openCloseDrawer(view: View){
+        if (drawerLayout.isDrawerOpen(GravityCompat.END)) {
+            drawerLayout.closeDrawer(GravityCompat.END)
+        } else {
+            drawerLayout.openDrawer(GravityCompat.END)
+        }
     }
-    fun setter(){
-        pageName.setText(menulist[pagenumber])
-        replaceFragment(R.id.fragmentContainerView, fragmentlist[pagenumber])
-    }
+
+    // Initialize views and variables
     fun init() {
         myView = findViewById(R.id.myView)
         pageName = findViewById(R.id.pageName)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        buttonOpenDrawer = findViewById(R.id.open_button)
         val pageNum = PageNum(this@MainActivity)
         var page = pageNum.getPageNum()
         if(page != null){
