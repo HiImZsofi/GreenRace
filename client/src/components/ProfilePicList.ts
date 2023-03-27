c8087ad81f0db5445ca01f60cf9e392c0943d8dd
Benedek Ákos <benedek.akos.marcell@gmail.com>
2023-03-27 15:32:47 +0200

    Change picture names to be android compatible

--- client/src/components/ProfilePicList.ts
+++ client/src/components/ProfilePicList.ts
@@ -6,8 +6,8 @@ export interface Profpics {
 
 const Profilepictures: Profpics[] = [
   {path: "npic.png", name: "semleges", unlock: true},
-  {path: "profpic-male.jpg", name: "hím", unlock: true},
-  {path: "profpic-female.jpg", name: "nőstény", unlock: true},
+  {path: "profpic_male.jpg", name: "hím", unlock: true},
+  {path: "profpic_female.jpg", name: "nőstény", unlock: true},
   {path: "golden.png", name: "traktor", unlock: false},
 ];
 
