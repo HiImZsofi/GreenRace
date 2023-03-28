deb0356897e63711e0e3db741760fa07339f774f
HiImZsofi <zsofia.gyori00@gmail.com>
2023-03-28 15:38:41 +0200

    First 500g save function inserting into database

--- server/src/achivementZso.js
+++ server/src/achivementZso.js
@@ -1,19 +1,22 @@
-import { getRouteData } from "./queries.js";
+import { getRouteData, insertNewAchievement } from "./queries.js";
 
 var routeData = await getRouteData("1"); //!remove hardcoded string
 
 // console.log(routeData);
 
-export function first500gEmission() {
+export async function first500gEmission() {
   var emissionSum = 0;
   for (let i = 0; i < routeData.length; i++) {
     emissionSum += routeData[i].emission;
   }
 
   if (emissionSum >= 500) {
-    //TODO insert into achivement table
+    await insertNewAchievement("1", routeData[0].user_id).catch((err) => {
+      throw err;
+    });
     return { completed: true };
   }
-
   return { completed: false };
 }
+
+console.log(first500gEmission());
