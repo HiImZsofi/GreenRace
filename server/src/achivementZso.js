df3236e933ee4b1630f626cb01a9cd26fc3536c8
HiImZsofi <zsofia.gyori00@gmail.com>
2023-03-29 13:27:02 +0200

    Add achievement progression to the return object

--- server/src/achivementZso.js
+++ server/src/achivementZso.js
@@ -1,22 +1,23 @@
 import { getRouteData, insertNewAchievement } from "./queries.js";
 
 var routeData = await getRouteData("1"); //!remove hardcoded string
+var completion;
 
 // console.log(routeData);
 
-export async function first500gEmission() {
+export async function first500gEmission(user_id) {
+  //* 1 id
   var emissionSum = 0;
   for (let i = 0; i < routeData.length; i++) {
     emissionSum += routeData[i].emission;
   }
 
   if (emissionSum >= 500) {
-    await insertNewAchievement("1", routeData[0].user_id).catch((err) => {
+    await insertNewAchievement(user_id, routeData[0].user_id).catch((err) => {
       throw err;
     });
-    return { completed: true };
+    return { completed: true, completion: "100" };
   }
-  return { completed: false };
+  completion = (emissionSum / 500) * 100;
+  return { completed: false, completion: completion };
 }
-
-console.log(first500gEmission());
