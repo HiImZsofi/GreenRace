992530d8c3f236b7991b5c9264e2c8c199237b90
HiImZsofi <zsofia.gyori00@gmail.com>
2023-03-29 14:56:50 +0200

    Added fifth and sixth achievement

--- server/src/achivementZso.js
+++ server/src/achivementZso.js
@@ -1,23 +1,71 @@
 import { getRouteData, insertNewAchievement } from "./queries.js";
 
-var routeData = await getRouteData("1"); //!remove hardcoded string
+var routeData;
 var completion;
 
 // console.log(routeData);
 
 export async function first500gEmission(user_id) {
   //* 1 id
+  routeData = await getRouteData(user_id);
   var emissionSum = 0;
+
+  //sum up the emission of every route the user registered
   for (let i = 0; i < routeData.length; i++) {
     emissionSum += routeData[i].emission;
   }
 
   if (emissionSum >= 500) {
-    await insertNewAchievement(user_id, routeData[0].user_id).catch((err) => {
+    await insertNewAchievement("1", routeData[0].user_id).catch((err) => {
       throw err;
     });
-    return { completed: true, completion: "100" };
+    return { completed: true, progress: "100" };
   }
+
+  //sends back the current completion if achievement is not done yet
   completion = (emissionSum / 500) * 100;
-  return { completed: false, completion: completion };
+  return { completed: false, progress: completion };
+}
+
+export async function atLeast2kgEmission(user_id) {
+  //*5 id
+  routeData = await getRouteData(user_id);
+  var emissionSum = 0;
+
+  for (let i = 0; i < routeData.length; i++) {
+    emissionSum += routeData[i].emission;
+  }
+
+  if (emissionSum >= 2000) {
+    await insertNewAchievement("5", routeData[0].user_id).catch((err) => {
+      throw err;
+    });
+    return { completed: true, progess: "100" };
+  }
+  completion = (emissionSum / 2000) * 100;
+  return { completed: false, progress: completion };
+}
+
+export async function onceOnEveryVehicleType(user_id) {
+  //*6 id
+  routeData = await getRouteData(user_id);
+  var vehicleTypes = [];
+
+  //push every type of vehicle the user travelled on into array
+  for (let i = 0; i < routeData.length; i++) {
+    if (!vehicleTypes.includes(routeData[i].route_type)) {
+      vehicleTypes.push(routeData[i].route_type);
+    }
+  }
+
+  //5 is the max number of vehicles
+  if (vehicleTypes.length == 5) {
+    await insertNewAchievement("6", routeData[0].user_id).catch((err) => {
+      throw err;
+    });
+    return { completed: true, progess: "100" };
+  }
+
+  completion = (vehicleTypes.length / 5) * 100;
+  return { completed: false, progress: completion };
 }
