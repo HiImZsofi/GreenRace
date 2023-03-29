aec952d04464a1be35eeaa87fa6b851de1b7c509
Gondi László <laszlo.zsolt.gondi@gmail.com>
2023-03-29 11:40:09 +0200

    Add atLeast3kmsTravelled function
    It checks if the user has travelled over 3 KMs based on his logged routes

--- server/src/achievement_lac.js
+++ server/src/achievement_lac.js
@@ -2,6 +2,7 @@ import { getRouteData } from "./queries.js";
 
 //First logged route achievement
 export async function firstLoggedRouteAchievement(user_Id) {
+	//*Achievement ID: 2
 	var getUserLoggedRoutes = await getRouteData(user_Id).catch((err) => {
 		//returns false for completion if the number of routes logged is 0
 		return { completed: false };
@@ -13,4 +14,19 @@ export async function firstLoggedRouteAchievement(user_Id) {
 		//returns false for completion if the number of routes logged is 0
 		return { completed: false };
 	}
+}
+
+//At least 3 kilometers of travel logged
+export async function atLeastThreeKilometersLoggedAchievement(user_Id) {
+	//*Achievement ID: 3
+	let getUserLoggedRoutes = await getRouteData(user_Id).catch((err) => {
+		return { completed: false };
+	});
+	let sum = 0;
+	getUserLoggedRoutes.forEach((element) => {
+		sum += element.length;
+	});
+	return sum >= 3
+		? console.log({ completed: true, progress: 100 })
+		: console.log({ completed: false, progress: (sum / 3.0) * 100 });
 }
