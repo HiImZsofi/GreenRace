c80a008bb62823eebae774c885e9ca0b98757ff4
Gondi László <laszlo.zsolt.gondi@gmail.com>
2023-03-31 13:11:59 +0200

    Fix typos

--- server/src/achievements.js
+++ server/src/achievements.js
@@ -63,192 +63,192 @@ export async function first500gEmission(user_id) {
 }
 
 export async function atLeastThreeKilometersLoggedAchievement(user_id) {
-  //*Achievement ID: 3
-  let routeData = await getRouteData(user_id).catch((err) => {
-    return { completed: false };
-  });
+	//*Achievement ID: 3
+	let routeData = await getRouteData(user_id).catch((err) => {
+		return { completed: false };
+	});
 
-  //The sum of the distance travelled based on the logged routes
-  let sum = 0;
-  routeData.forEach((route) => {
-    sum += route.length;
-  });
+	//The sum of the distance travelled based on the logged routes
+	let sum = 0;
+	routeData.forEach((route) => {
+		sum += route.length;
+	});
 
-  //Return true with a 100% progess if the sum is more than 3 kms
-  //Return false if the sum is less than 3
-  //and also calculate the current progress if the latter is true
-  return sum >= 3
-    ? {
-        name: "Három a magyar igazság",
-        description: "Utazz több mint 3 kilométert",
-        completed: true,
-        progress: 100,
-      }
-    : {
-        name: "Három a magyar igazság",
-        description: "Utazz több mint 3 kilométert",
-        completed: false,
-        progress: (sum / 3.0) * 100,
-      };
+	//Return true with a 100% progress if the sum is more than 3 kms
+	//Return false if the sum is less than 3
+	//and also calculate the current progress if the latter is true
+	return sum >= 3
+		? {
+				name: "Három a magyar igazság",
+				description: "Utazz több mint 3 kilométert",
+				completed: true,
+				progress: 100,
+		  }
+		: {
+				name: "Három a magyar igazság",
+				description: "Utazz több mint 3 kilométert",
+				completed: false,
+				progress: (sum / 3.0) * 100,
+		  };
 }
 
 export async function atLeastThreeDifferentTypesTravelledOn(user_id) {
-  //*Achievement ID: 4
-  const routeData = await getRouteData(user_id);
+	//*Achievement ID: 4
+	const routeData = await getRouteData(user_id);
 
-  //Calculate the number of different types logged
-  //The size contains the count of the types of transport
-  const types = routeData.reduce((types, obj) => {
-    types.add(obj.route_type);
-    return types;
-  }, new Set());
+	//Calculate the number of different types logged
+	//The size contains the count of the types of transport
+	const types = routeData.reduce((types, obj) => {
+		types.add(obj.route_type);
+		return types;
+	}, new Set());
 
-  //If the size is 3 or more then it returns true for the completed key's value
-  //If it is less than that then returns false with the current progress
-  return types.size >= 3
-    ? {
-        name: "Változatosság",
-        description: "Utazz három különböző fajta járművön",
-        completed: true,
-        progress: 100,
-      }
-    : {
-        name: "Változatosság",
-        description: "Utazz három különböző fajta járművön",
-        completed: false,
-        progress: (types.size / 3) * 100,
-      };
+	//If the size is 3 or more then it returns true for the completed key's value
+	//If it is less than that then returns false with the current progress
+	return types.size >= 3
+		? {
+				name: "Változatosság",
+				description: "Utazz három különböző fajta járművön",
+				completed: true,
+				progress: 100,
+		  }
+		: {
+				name: "Változatosság",
+				description: "Utazz három különböző fajta járművön",
+				completed: false,
+				progress: (types.size / 3) * 100,
+		  };
 }
 
 export async function atLeast2kgEmission(user_id) {
-  //*5 id
-  routeData = await getRouteData(user_id);
-  var emissionSum = 0;
+	//*5 id
+	routeData = await getRouteData(user_id);
+	var emissionSum = 0;
 
-  for (let i = 0; i < routeData.length; i++) {
-    emissionSum += routeData[i].emission;
-  }
+	for (let i = 0; i < routeData.length; i++) {
+		emissionSum += routeData[i].emission;
+	}
 
-  if (emissionSum >= 2000) {
-    await insertNewAchievement("5", routeData[0].user_id).catch((err) => {
-      throw err;
-    });
-    return {
-      name: "Környezet tudatos",
-      description: "Spórolj meg legalább 2000g szén-dioxidot",
-      completed: true,
-      progess: 100,
-    };
-  }
-  completion = (emissionSum / 2000) * 100;
-  return {
-    name: "Környezet tudatos",
-    description: "Spórolj meg legalább 2000g szén-dioxidot",
-    completed: false,
-    progress: completion,
-  };
+	if (emissionSum >= 2000) {
+		await insertNewAchievement("5", routeData[0].user_id).catch((err) => {
+			throw err;
+		});
+		return {
+			name: "Környezet tudatos",
+			description: "Spórolj meg legalább 2000g szén-dioxidot",
+			completed: true,
+			progress: 100,
+		};
+	}
+	completion = (emissionSum / 2000) * 100;
+	return {
+		name: "Környezet tudatos",
+		description: "Spórolj meg legalább 2000g szén-dioxidot",
+		completed: false,
+		progress: completion,
+	};
 }
 
 export async function onceOnEveryVehicleType(user_id) {
-  //*6 id
-  routeData = await getRouteData(user_id);
-  var vehicleTypes = [];
+	//*6 id
+	routeData = await getRouteData(user_id);
+	var vehicleTypes = [];
 
-  //push every type of vehicle the user travelled on into array
-  for (let i = 0; i < routeData.length; i++) {
-    if (!vehicleTypes.includes(routeData[i].route_type)) {
-      vehicleTypes.push(routeData[i].route_type);
-    }
-  }
+	//push every type of vehicle the user travelled on into array
+	for (let i = 0; i < routeData.length; i++) {
+		if (!vehicleTypes.includes(routeData[i].route_type)) {
+			vehicleTypes.push(routeData[i].route_type);
+		}
+	}
 
-  //5 is the max number of vehicles
-  if (vehicleTypes.length == 5) {
-    await insertNewAchievement("6", routeData[0].user_id).catch((err) => {
-      throw err;
-    });
-    return {
-      name: "Kíváncsi",
-      description:
-        "Rögzíts egy utat az összes járműtípuson (HÉV, busz, troli, stb.)",
-      completed: true,
-      progess: 100,
-    };
-  }
+	//5 is the max number of vehicles
+	if (vehicleTypes.length == 5) {
+		await insertNewAchievement("6", routeData[0].user_id).catch((err) => {
+			throw err;
+		});
+		return {
+			name: "Kíváncsi",
+			description:
+				"Rögzíts egy utat az összes járműtípuson (HÉV, busz, troli, stb.)",
+			completed: true,
+			progress: 100,
+		};
+	}
 
-  completion = (vehicleTypes.length / 5) * 100;
-  return {
-    name: "Kíváncsi",
-    description:
-      "Rögzíts egy utat az összes járműtípuson (HÉV, busz, troli, stb.)",
-    completed: false,
-    progress: completion,
-  };
+	completion = (vehicleTypes.length / 5) * 100;
+	return {
+		name: "Kíváncsi",
+		description:
+			"Rögzíts egy utat az összes járműtípuson (HÉV, busz, troli, stb.)",
+		completed: false,
+		progress: completion,
+	};
 }
 
 export async function atLeastTenKilometersInOneLog(user_id) {
-  //*Achievement ID: 7
-  const routeData = await getRouteData(user_id).catch((err) => {
-    return { completed: false };
-  });
+	//*Achievement ID: 7
+	const routeData = await getRouteData(user_id).catch((err) => {
+		return { completed: false };
+	});
 
-  //Sets the initial status for the completion which is false
-  let completedStatus = {
-    name: "Kitartó",
-    description: "Tegyél meg több mint 10 kilométert egy utazás alatt",
-    completed: false,
-    progress: 0,
-  };
+	//Sets the initial status for the completion which is false
+	let completedStatus = {
+		name: "Kitartó",
+		description: "Tegyél meg több mint 10 kilométert egy utazás alatt",
+		completed: false,
+		progress: 0,
+	};
 
-  routeData.forEach((route) => {
-    if (route.length >= 10) {
-      //Sets the completion status to true if the route l
-      completedStatus = {
-        name: "Kitartó",
-        description: "Tegyél meg több mint 10 kilométert egy utazás alatt",
-        completed: true,
-        progress: 100,
-      };
-    }
-  });
-  return completedStatus;
+	routeData.forEach((route) => {
+		if (route.length >= 10) {
+			//Sets the completion status to true if the route l
+			completedStatus = {
+				name: "Kitartó",
+				description: "Tegyél meg több mint 10 kilométert egy utazás alatt",
+				completed: true,
+				progress: 100,
+			};
+		}
+	});
+	return completedStatus;
 }
 
 export async function fromOneEndToAnother(user_id) {
-  //*8 id
-  routeData = await getRouteData(user_id);
-  var linestops;
+	//*8 id
+	routeData = await getRouteData(user_id);
+	var linestops;
 
-  let completedStatus = {
-    name: "Elkötelezett",
-    description: "Menj végig bármelyik vonalon végállomástól végállomásig",
-    completed: false,
-    progress: 0,
-  };
+	let completedStatus = {
+		name: "Elkötelezett",
+		description: "Menj végig bármelyik vonalon végállomástól végállomásig",
+		completed: false,
+		progress: 0,
+	};
 
-  for (let i = 0; i < routeData.length; i++) {
-    linestops = await getLongerRoute(routeData[i].route_id);
-    for (let j = 0; j < linestops.length; j++) {
-      if (
-        (linestops[0].stopname === routeData[i].onstop ||
-          linestops[0].stopname === routeData[i].offstop) &&
-        (linestops[linestops.length - 1].stopname === routeData[i].onstop ||
-          linestops[linestops.length - 1].stopname === routeData[i].offstop)
-      ) {
-        await insertNewAchievement("8", routeData[0].user_id).catch((err) => {
-          throw err;
-        });
-        return {
-          name: "Elkötelezett",
-          description:
-            "Menj végig bármelyik vonalon végállomástól végállomásig",
-          completed: true,
-          progess: 100,
-        };
-      }
-    }
-  }
+	for (let i = 0; i < routeData.length; i++) {
+		linestops = await getLongerRoute(routeData[i].route_id);
+		for (let j = 0; j < linestops.length; j++) {
+			if (
+				(linestops[0].stopname === routeData[i].onstop ||
+					linestops[0].stopname === routeData[i].offstop) &&
+				(linestops[linestops.length - 1].stopname === routeData[i].onstop ||
+					linestops[linestops.length - 1].stopname === routeData[i].offstop)
+			) {
+				await insertNewAchievement("8", routeData[0].user_id).catch((err) => {
+					throw err;
+				});
+				return {
+					name: "Elkötelezett",
+					description:
+						"Menj végig bármelyik vonalon végállomástól végállomásig",
+					completed: true,
+					progress: 100,
+				};
+			}
+		}
+	}
 
-  return completedStatus;
+	return completedStatus;
 }
 
 export async function travelFiftyKilometers(user_id) {
